8f6bdb7b614712e07f06c4f7394d6e89d9858393
Valentin Vasile <work@vasile.digital>
2023-09-29 17:51:00 +0200

    implemented lobby with codes logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ const App = () => {
   const [playerName, setPlayerName] = useState('');
   const [players, setPlayers] = useState([]);
   const [isHost, setIsHost] = useState(false); // Track if the current user is the host
+  const [isCreatingGame, setIsCreatingGame] = useState(false); // Track if the user wants to create a game
+  const [gameId, setGameId] = useState(''); // Track the game ID
 
   // Initialize socket connection
   useEffect(() => {
@@ -23,6 +25,17 @@ const App = () => {
       setIsHost(true);
     });
 
+    // Handle when the game is created and a game ID is provided
+    socket.on('game-created', (createdGameId) => {
+      setGameId(createdGameId);
+      setIsCreatingGame(false); // Stop showing game creation elements
+    });
+
+    // Handle when the game is not found (invalid game ID)
+    socket.on('game-not-found', () => {
+      alert('Game not found. Please check the game ID.');
+    });
+
     return () => {
       socket.disconnect();
     };
@@ -36,26 +49,47 @@ const App = () => {
     }
   };
 
-  // Event handler for the player joining the game
+  // Event handler for creating a game
+  const createGame = () => {
+    setIsCreatingGame(true);
+    socket.emit('create-game', playerName); // Inform the server to create a game
+  };
+
+  // Event handler for joining a game with a game ID
   const joinGame = () => {
-    socket.emit('join-game', playerName); // Inform the server that the player is joining with their name
+    socket.emit('join-game', gameId, playerName); // Inform the server that the player is joining with their name and game ID
   };
 
   return (
     <div>
-      {isHost ? (
+      {isCreatingGame ? (
         <div>
-          <button onClick={startGame}>Start Game</button>
+          {/* Render game creation elements here */}
         </div>
       ) : (
         <div>
-          <input
-            type="text"
-            placeholder="Your Name"
-            value={playerName}
-            onChange={(e) => setPlayerName(e.target.value)}
-          />
+          {isHost ? (
+            <div>
+              <button onClick={startGame}>Start Game</button>
+            </div>
+          ) : (
+            <div>
+              <input
+                type="text"
+                placeholder="Your Name"
+                value={playerName}
+                onChange={(e) => setPlayerName(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Game ID"
+                value={gameId}
+                onChange={(e) => setGameId(e.target.value)}
+              />
+            </div>
+          )}
           <button onClick={joinGame}>Join Game</button>
+          <button onClick={createGame}>Create Game</button>
         </div>
       )}
 
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const { nanoid } = require('nanoid'); // Import nanoid for generating unique game IDs
 
 const app = express();
 const server = http.createServer(app);
@@ -9,11 +10,11 @@ const io = socketIo(server);
 const PORT = process.env.PORT || 3000;
 
 // Store game state
+let games = {}; // Store games by ID
 let players = [];
 let words = [];
 let faker = null;
 let round = 0;
-let host = null; // Track the host player
 
 // Serve static files (if you have a client-side game interface)
 app.use(express.static('public'));
@@ -21,42 +22,73 @@ app.use(express.static('public'));
 // Socket.io logic for game
 
 io.on('connection', (socket) => {
-  // Handle player joining
-  socket.on('join-game', (playerName) => {
+  // Handle player joining a game
+  socket.on('join-game', (gameId, playerName) => {
+    // Check if the game exists
+    if (!games[gameId]) {
+      socket.emit('game-not-found');
+      return;
+    }
+
     // Add the player to the game
-    players.push({ id: socket.id, name: playerName });
+    const game = games[gameId];
+    game.players.push({ id: socket.id, name: playerName });
+    console.log('player ' + playerName + ' joined game ' + gameId);
 
     // If no host is set, make the current player the host
-    if (!host) {
-      host = socket.id;
+    if (!game.host) {
+      game.host = socket.id;
       socket.emit('you-are-the-host');
+      console.log('host set for game ' + gameId + ': ' + game.host);
     }
 
     // Send game information to the player
     socket.emit('game-info', {
-      players,
-      words,
-      round,
-      isFaker: faker === socket.id,
+      players: game.players,
+      words: game.words,
+      round: game.round,
+      isFaker: game.faker === socket.id,
     });
 
-    // Broadcast to other players that a new player has joined
-    socket.broadcast.emit('player-joined', playerName);
-    console.log(playerName + " joined.")
+    // Broadcast to other players in the same game that a new player has joined
+    io.to(gameId).emit('player-joined', playerName);
+        console.log(playerName + " joined game " + gameId);
+  });
+
+  // Handle creating a new game and getting a game ID
+  socket.on('create-game', (playerName) => {
+    const gameId = nanoid(6); // Generate a unique game ID
+    games[gameId] = {
+      players: [{ id: socket.id, name: playerName }],
+      words: [], // You can initialize words here
+      faker: null,
+      round: 0,
+      host: socket.id,
+    };
+    socket.emit('game-created', gameId);
+    console.log('Game ' + gameId + ' created by ' + playerName);
+    socket.join(gameId); // Join the room with the game ID
   });
 
   // Handle game start by the host
   socket.on('start-game', () => {
-    if (socket.id === host) {
+    // Find the game that the host is in
+    const gameId = Object.keys(games).find(
+      (gameId) => games[gameId].host === socket.id
+    );
+
+    if (gameId) {
+      const game = games[gameId];
       // Randomly assign roles and words to players here
       // Set faker and words accordingly
       // Start the first round
-      io.emit('game-started', {
-        players,
-        words,
-        round,
-        faker,
+      io.to(gameId).emit('game-started', {
+        players: game.players,
+        words: game.words,
+        round: game.round,
+        faker: game.faker,
       });
+      console.log('game ' + gameId + ' started.');
     }
   });
 
@@ -64,20 +96,31 @@ io.on('connection', (socket) => {
   socket.on('end-game', ({ fakerCaught }) => {
     // Calculate and display final scores here
     // Update the game state for a new round or game
+    console.log('game ended');
   });
 
   // Handle disconnect
   socket.on('disconnect', () => {
-    // Remove the player from the game
-    players = players.filter((player) => player.id !== socket.id);
+    // Find the game that the disconnected player is in
+    const gameId = Object.keys(games).find(
+      (gameId) => games[gameId].players.some((player) => player.id === socket.id)
+    );
 
-    // If the host disconnects, choose a new host
-    if (socket.id === host) {
-      host = players.length > 0 ? players[0].id : null;
+    if (gameId) {
+      const game = games[gameId];
+      // Remove the player from the game
+      game.players = game.players.filter((player) => player.id !== socket.id);
+
+      // If the host disconnects, choose a new host
+      if (socket.id === game.host) {
+        game.host = game.players.length > 0 ? game.players[0].id : null;
+        console.log('host disconnected for game ' + gameId + ', new host now is: ' + game.host);
+      }
+
+      // Broadcast to other players in the same game that a player has left
+      socket.to(gameId).broadcast.emit('player-left', socket.id);
+      console.log('player ' + socket.id +  ' has left game ' + gameId);
     }
-
-    // Broadcast to other players that a player has left
-    socket.broadcast.emit('player-left', socket.id);
   });
 });
 
